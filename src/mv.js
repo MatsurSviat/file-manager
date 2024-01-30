2ad6be2a1f9e8a7eb0733aff2cbccd82edf934c8
Matsur Sviataslau <sviatdeveloper@gmail.com>
2024-01-30 20:59:24 +0300

    feat: finish file manager

--- src/mv.js
+++ src/mv.js
@@ -1,16 +1,32 @@
 import { __dirname } from "../index.js";
-import { copyFile, rm } from "fs/promises";
 
 import path from "path";
 
-function mv(...params) {
+async function mv(...params) {
   try {
-    let newDirname = path.resolve(__dirname, params[1]);
-    let pathFile = path.resolve(__dirname, params[0]);
-    copyFile(pathFile, newDirname);
-    rm(pathFile);
+    const sourcePath = path.resolve(__dirname, params[0]);
+    const destinationPath = path.resolve(__dirname, params[1]);
+
+    const readStream = createReadStream(sourcePath);
+    const writeStream = createWriteStream(destinationPath);
+
+    readStream.pipe(writeStream);
+
+    readStream.on('end', async () => {
+      try {
+        await unlink(sourcePath);
+        console.log('File moved successfully');
+      } catch (error) {
+        console.error('Error removing source file:', error);
+      }
+    });
+
+    readStream.on('error', (error) => {
+      console.error('Stream error:', error);
+    });
+
   } catch (error) {
-    console.error("Operation failed");
+    console.error('Operation failed:', error);
   }
 }
 
